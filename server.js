73a57873d4e031a93258ebdbd54f6286e603ed9c
Colin Stevens <colinstevens06@gmail.com>
2020-01-21 22:18:18 -0500

    updated port for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 // Setting up Express App
 const app = express();
-const PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 // Setting up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
